996b3ad1f2aa365258929ba0f38fbbc2feafa9fe
ilyapoborcev <ilyapoborcev@gmail.com>
2015-11-10 15:37:06 +0300

    easy version of test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,8 @@
-sudo echo "delay = 5"
-sudo echo "5" > /sys/repeat_hello/delay
-echo "Wait for 15 sec"
-sleep 15
+sudo echo "delay = 2"
+sudo echo "2" > /dev/hello
+echo "Wait for 11 sec"
+sleep 11
 dmesg | tail 
 
-sudo echo "delay = 2"
-sudo echo "2" > /sys/repeat_hello/delay
-echo "Wait for 11 sec"
-sleep 10
-
-sudo echo "0" > /sys/repeat_hello/delay
+sudo echo "0" > /dev/hello
 dmesg | tail
